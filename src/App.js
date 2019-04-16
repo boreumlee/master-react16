7a364b3807b3c7a3be0338c126acd36a6161b7b3
Luna <boreum4151@gmail.com>
2019-04-16 19:07:10 +0900

    use setstate null

--- src/App.js
+++ src/App.js
@@ -1,71 +1,33 @@
 import React, { Component, Fragment } from 'react';
-import {createPortal} from 'react-dom'
 
-const BoundaryHOC = ProtectedComponent => class Boundary extends Component {
-  state={
-    hasError: false
-  }
-  componentDidCatch = () =>{
-    this.setState({
-      hasError:true
-    })
-  }
-  render(){
-    const {hasError} = this.state
-    if(hasError){
-      return <ErrorFallback />
-    } else {
-      return <ProtectedComponent />
-    }
-  }
-}
+const MAX_PIZZAS = 20;
 
-class ErrorMaker extends Component{
-  state={
-    friends: ["aa","bb","xx","dd"]
+const eatPizza = (state, props) => {
+  const {pizzas} = state;
+  if(pizzas < MAX_PIZZAS){
+    return {pizzas : pizzas+1};
+  } else{
+    return null
+  }
+};
+
+class Controlled extends Component{
+  state = {
+    pizzas : 10
   };
-  componentDidMount = () =>{
-    setTimeout(()=>{
-      this.setState({
-        friends:undefined
-      }) // 이렇게 state의 friends가 undefined가 되거나 데이터들이 사라지는 경우가 굉장이 허다함
-    }, 2000);
-  }
   render(){
-    //state에서 friends를 가져와 array로 만든것
-    const {friends} = this.state;
-    return friends.map(friend => ` ${friend} `)
-  }
+    const {pizzas} = this.state;
+    return (<button onClick={this._handleClick}>{`I have eaten ${pizzas} ${pizzas === 1 ? `pizza` : `pizzas`}`}</button>);
+  };
+  _handleClick = () =>{
+    this.setState(eatPizza);
+  };
 }
-
-const PErrorMaker = BoundaryHOC(ErrorMaker)
-
-class Portals extends Component{
-  render(){
-    return createPortal(<Message />, document.getElementById("touch"))
-  }
-}
-
-const Message = () => "Just touched this!"
-
-class ReturnTypes extends Component {
-  render(){
-    return "Hello~~~";
-  }
-}
-
-const PPortals = BoundaryHOC(Portals)
-
-const ErrorFallback = () => "Sorry something went wrong";
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <ReturnTypes/>
-        <PPortals/>
-        <PErrorMaker />
-      </Fragment>
+      <Controlled />
     );
   }
 }
